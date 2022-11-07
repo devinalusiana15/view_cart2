25a36316b34e41fb25d9e21ab9b9c98e12a557fb
FathanFalatansya <fathanfalatansya31@gmail.com>
2022-11-07 11:55:41 +0700

    Update method

--- src/views/LihatKeranjang.vue
+++ src/views/LihatKeranjang.vue
@@ -4,16 +4,17 @@
       <CarouselVue />
     </div>
     <!-- STRUK -->
-    <div class="col-md-4 my-4">
+    
+    <div class="col-md-4 my-4" >
       <div class="card mx-5 my-3 px-3" id="struk" style="display:block; position: sticky; top: 80px;">
           <ul class="list-group list-group-flush justify-content-start" style="width: 100%">
             <li class="list-group-item" style="background-color: #fbf2bf; ">
               <h5 class="my-3"> Price Details</h5>
-              <h6 style="font-weight:normal">Sub Total &emsp;&nbsp; Rp. 140.000</h6>
-              <h6 style="font-weight:normal">PPN 10% &emsp;&nbsp; Rp. 14.000</h6>
+              <h6 style="font-weight:normal">Sub Total &emsp;&nbsp; Rp. {{carts.total_harga}} </h6>
+              <h6 style="font-weight:normal">PPN 10% &emsp;&nbsp; Rp. {{carts.total_harga*0.1}}</h6>
             </li>
             <li class="list-group-item" style="background-color: #fbf2bf">
-              <h6 style="font-weight:bold"> Total &emsp;&emsp;&emsp; Rp. 154.000</h6>
+              <h6 style="font-weight:bold"> Total &emsp;&emsp;&emsp; Rp. {{carts.total_harga+carts.total_harga*0.1}}</h6>
               <div class="d-grid">
                 <button class="btn btn-success my-3" type="button">Checkout</button>
               </div>
@@ -28,46 +29,89 @@
     <div class="col-md-8 my-4">
       <div class="container-fluid">
         <section class="carts">
-          <div v-for="produk in carts" :key="produk.id" class="produk">
-            <div class="card mt-3" style="width: 60%">
-              <div class="row g-1">
-                <div class="col-md-4 ml-2">
-                  <img
-                    src="../assets/Rectangle4.svg"
-                    class="img-fluid"
-                    style="width: 100%; height: 100%"
-                  />
-                </div>
-                <div class="col-md-8" :key="produk.id">
-                  <div class="card-body">
-                    <h5 class="card-title">{{ produk.nama_makanan }}</h5>
-                    <p class="card-text">
-                      {{ produk.harga_makanan }}
-                      <br />
-
-                      <span
-                        class="badge"
-                        type="button"
-                        @click="updateCart(produk, 'tambah')"
-                        style="backgorund-color: #59cfd7"
-                        >+</span
-                      >
-                      <span class="badge" style="background-color:#fbf2bf ">{{
-                        produk.kuantitas
-                      }}</span>
-                      <span
-                        class="badge"
-                        type="button"
-                        @click="updateCart(produk, 'kurang')"
-                        style="backgorund-color: #59cfd7"
-                        >-</span
-                      >
-                    </p>
-                  </div>
-                </div>
+          <div v-for="item in carts.produk" :key="item.nama_produk" class="produk">
+            <div class="mb-5 row justify-content-center ">
+              <div class="col">
+                <img
+                      src="../assets/Rectangle4.svg"
+                      class="img-fluid"
+                      style="width: 100%; height: 100%"
+                    />
               </div>
+              <div class="col">
+                <h5 class="card-title">{{ item.nama_produk }}</h5>
+                              <p class="col-md-12 card-text">
+                          {{ item.harga_produk }}
+                          <br />
+
+                          <span
+                            class="badge"
+                            type="button"
+                            @click="updateCart(item, 'kurang')"
+                            style="backgorund-color: #59cfd7"
+                            >-</span
+                          >
+                          <span class="badge" style="background-color:#fbf2bf ">{{ item.kuantitas }}</span>
+                          <span
+                            class="badge"
+                            type="button"
+                            @click="updateCart(item, 'tambah')"
+                            style="backgorund-color: #59cfd7"
+                            >+</span
+                          >
+                          </p>
+              </div>
+                  <div class="col">
+                  <br />
+                      <span class="badge" style="background-color:#fbf2bf; font-weight: 400; width: 199px; height: 30px;">Rp. {{item.harga_produk*item.kuantitas }}</span>
+                  </div>
             </div>
-          </div>
+              <!-- <div class="card mt-3" style="width: 60%">
+                <div class="row g-1">
+                  <div class="col-md-4 ml-2">
+                    <img
+                      src="../assets/Rectangle4.svg"
+                      class="img-fluid"
+                      style="width: 100%; height: 100%"
+                    />
+                  </div>
+                  <div class="col-md-8">
+                    <div class="card-body">
+                      
+                        <div class="row justify-content-cente align-items-center">
+
+                          <div class="col">
+                            <h5 class="card-title">{{ item.nama_produk }}</h5>
+                              <p class="col-md-12 card-text">
+                          {{ item.harga_produk }}
+                          <br />
+
+                          <span
+                            class="badge"
+                            type="button"
+                            @click="updateCart(item, 'tambah')"
+                            style="backgorund-color: #59cfd7"
+                            >+</span
+                          >
+                          <span class="badge" style="background-color:#fbf2bf ">{{ item.kuantitas }}</span>
+                          <span
+                            class="badge"
+                            type="button"
+                            @click="updateCart(item, 'kurang')"
+                            style="backgorund-color: #59cfd7"
+                            >-</span
+                          >
+                          </p>
+                          </div>
+                          <div class="col">
+                            <span class="badge" style="background-color:#fbf2bf; font-weight: 400;">Rp. {{item.harga_total }}</span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div> -->
+            </div>
         </section>
         <br><br>
       </div>
@@ -77,51 +121,13 @@
 
 <script>
 import CarouselVue from "../components/CarouselVue.vue";
+import axios from "axios";
 
 export default {
   name: "LihatKeranjang",
   data() {
     return {
-      carts: [
-        {
-          id: 1,
-          nama_makanan: "Udang Mentega",
-          harga_makanan: "35.000",
-          kuantitas: 1,
-        },
-
-        {
-          id: 2,
-          nama_makanan: "Udang Mentega Pedas",
-          harga_makanan: "35.000",
-          kuantitas: 1,
-        },
-
-        {
-          id: 3,
-          nama_makanan: "Udang Mentega Manis",
-          harga_makanan: "35.000",
-          kuantitas: 1,
-        },
-        {
-          id: 4,
-          nama_makanan: "Udang Mentega Manis",
-          harga_makanan: "35.000",
-          kuantitas: 1,
-        },
-        {
-          id: 5,
-          nama_makanan: "Udang Mentega Manis",
-          harga_makanan: "35.000",
-          kuantitas: 1,
-        },
-        {
-          id: 6,
-          nama_makanan: "Udang Mentega Manis",
-          harga_makanan: "35.000",
-          kuantitas: 1,
-        },
-      ],
+      carts: [],
     };
   },
   computed: {
@@ -130,26 +136,38 @@ export default {
     },
   },
   methods: {
-    updateCart(produk, updateType) {
-      for (let i = 0; i < this.carts.length; i++) {
-        if (this.carts[i].id === produk.id) {
-          if (updateType === "kurang") {
-            if (this.carts[i].kuantitas !== 0) {
-              this.carts[i].kuantitas--;
-            }
-          } else {
-            this.carts[i].kuantitas++;
-          }
+      updateCart(item, updateType) {      
+              if (updateType === 'kurang') {
+                if (item.kuantitas !== 0) {
+                  item.kuantitas--;
+                }
+              } else {
+               item.kuantitas++;
+              }
+            },
 
-          break;
-        }
-      }
-      console.log(this.carts);
-    },
+      setproduk(data){
+         this.carts = data.cart
+      },
+
+      chekOut(){
+        axios
+        .patch("http://localhost:3000/api/cart/635f4f806c23111df8ddfe3e",{
+          productName:this.carts.nama_produk,
+          kuantitas:this.carts.kuantitas
+        })
+      }  
   },
 
   components: {
     CarouselVue: CarouselVue,
+  },
+
+  mounted(){
+    axios
+    .get("http://localhost:3000/api/cart/635f4f806c23111df8ddfe3e")
+     .then((response) => this.setproduk(response.data))
+    // .then(response => console.log(response.data.cart.produk[0].nama_produk))
   },
 };
 </script>
